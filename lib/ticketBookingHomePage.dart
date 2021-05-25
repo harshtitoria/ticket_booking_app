2c89aea288f7a2932dbf47094b74eecea47455aa
harshtitoria <harsh.titoria03@gmail.com>
2021-05-26 01:12:43 +0530

    horizontal list view of column containers set up

--- lib/ticketBookingHomePage.dart
+++ lib/ticketBookingHomePage.dart
@@ -67,6 +67,7 @@ class _TicketBookingHomePageState extends State<TicketBookingHomePage> {
                       Expanded(
                         flex: 3,
                         child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             Column(
                               children: [
@@ -177,13 +178,265 @@ class _TicketBookingHomePageState extends State<TicketBookingHomePage> {
                   Container(
                     height: MediaQuery.of(context).size.height / 4,
                     decoration: BoxDecoration(color: Colors.white),
-                    padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+                    padding: EdgeInsets.fromLTRB(17, 8, 0, 8),
                     child: Column(children: [
                       Expanded(
-                        child: Placeholder(),
+                        flex: 2,
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 17),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Upcoming Trip',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 1,
+                                      ),
+                                    ),
+                                    Text(
+                                      'You have 2 upcoming ticket',
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  'See All',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.greenAccent,
+                                  ),
+                                ),
+                              ]),
+                        ),
                       ),
                       Expanded(
-                        child: Placeholder(),
+                        flex: 5,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width / 1.5,
+                              margin: EdgeInsets.only(right: 15),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.grey.shade200,
+                                ),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              padding: EdgeInsets.all(13),
+                              child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(
+                                          Icons.train,
+                                          color: Colors.orange[350],
+                                        ),
+                                        Text(
+                                          'Train',
+                                          style: TextStyle(
+                                            color: Colors.orange[300],
+                                          ),
+                                        ),
+                                        Spacer(),
+                                        Text(
+                                          '2 Seats',
+                                          style: TextStyle(
+                                            color: Colors.orange[200],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Divider(
+                                      color: Colors.grey.shade300,
+                                      thickness: 1.7,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Column(
+                                          children: [
+                                            Text(
+                                              'MLG',
+                                              style: TextStyle(
+                                                color: Colors.grey,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 5),
+                                              child: Text(
+                                                '03:18',
+                                                style: TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                            ),
+                                            Text(
+                                              'May 31, 2021',
+                                              style: TextStyle(fontSize: 12),
+                                            )
+                                          ],
+                                        ),
+                                        Icon(
+                                          Icons.arrow_forward,
+                                          color: Colors.greenAccent,
+                                        ),
+                                        Column(
+                                          children: [
+                                            Text(
+                                              'JKT',
+                                              style: TextStyle(
+                                                color: Colors.grey,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 5),
+                                              child: Text(
+                                                '08:40',
+                                                style: TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                            ),
+                                            Text(
+                                              'May 31, 2021',
+                                              style: TextStyle(fontSize: 12),
+                                            )
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ]),
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width / 1.5,
+                              margin: EdgeInsets.only(right: 15),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.grey.shade200,
+                                ),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              padding: EdgeInsets.all(13),
+                              child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(
+                                          Icons.train,
+                                          color: Colors.orange[350],
+                                        ),
+                                        Text(
+                                          'Train',
+                                          style: TextStyle(
+                                            color: Colors.orange[300],
+                                          ),
+                                        ),
+                                        Spacer(),
+                                        Text(
+                                          '2 Seats',
+                                          style: TextStyle(
+                                            color: Colors.orange[200],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Divider(
+                                      color: Colors.grey.shade300,
+                                      thickness: 1.7,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Column(
+                                          children: [
+                                            Text(
+                                              'MLG',
+                                              style: TextStyle(
+                                                color: Colors.grey,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 5),
+                                              child: Text(
+                                                '03:18',
+                                                style: TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                            ),
+                                            Text(
+                                              'May 31, 2021',
+                                              style: TextStyle(fontSize: 12),
+                                            )
+                                          ],
+                                        ),
+                                        Icon(
+                                          Icons.arrow_forward,
+                                          color: Colors.greenAccent,
+                                        ),
+                                        Column(
+                                          children: [
+                                            Text(
+                                              'JKT',
+                                              style: TextStyle(
+                                                color: Colors.grey,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 5),
+                                              child: Text(
+                                                '08:40',
+                                                style: TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                            ),
+                                            Text(
+                                              'May 31, 2021',
+                                              style: TextStyle(fontSize: 12),
+                                            )
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ]),
+                            ),
+                          ],
+                        ),
                       ),
                     ]),
                   ),
@@ -191,6 +444,48 @@ class _TicketBookingHomePageState extends State<TicketBookingHomePage> {
                   Container(
                     height: MediaQuery.of(context).size.height / 4,
                     decoration: BoxDecoration(color: Colors.white),
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          flex: 3,
+                          child: Column(
+                            children: [
+                              Text(
+                                'Top destinations',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                'Recommended Place for you',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 10,
+                          child: ListView.builder(
+                            itemBuilder: (BuildContext context, int index) {
+                              return Container(
+                                width: 160,
+                                margin: EdgeInsets.only(right: 8),
+                                decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
